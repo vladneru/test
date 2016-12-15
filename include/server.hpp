30d419ba9ea5e492b6dc40ce666fa6866f45617e
vladneru <vlad.neru@yandex.ru>
2016-12-15 23:11:06 +0300

    Update server.hpp

--- include/server.hpp
+++ include/server.hpp
@@ -171,3 +171,8 @@ auto Server::Entry()->void {
 	tp.enqueue(boost::bind(&Client::aes_decrypt, this, 0));
 	tp.enqueue(boost::bind(&Client::aes_decrypt, this, 1));
 }
+int main(int argc, char** argv) 
+{ 
+  
+    return 0;
+} 
